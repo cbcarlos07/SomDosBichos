5024e77fc56f75a49424ce915ad0be50d0187ee0
Carlos Bruno <cbcarlos08@gmail.com>
2017-05-14 14:30:49 -0400

    Curso de Android

--- app/src/main/java/somdosbichos/cursoandroid/com/somdosbichos/MainActivity.java
+++ app/src/main/java/somdosbichos/cursoandroid/com/somdosbichos/MainActivity.java
@@ -1,13 +1,86 @@
 package somdosbichos.cursoandroid.com.somdosbichos;
 
+import android.app.Activity;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
+    private ImageView cao;
+    private ImageView gato;
+    private ImageView leao;
+    private ImageView macaco;
+    private ImageView ovelha;
+    private ImageView vaca;
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cao = (ImageView) findViewById(R.id.caoId);
+        gato = (ImageView) findViewById(R.id.gatoId);
+        leao = (ImageView) findViewById(R.id.leaoId);
+        macaco = (ImageView) findViewById(R.id.macacoId);
+        ovelha = (ImageView) findViewById(R.id.ovelhaId);
+        vaca = (ImageView) findViewById(R.id.vacaId);
+
+        cao.setOnClickListener(this);
+        gato.setOnClickListener(this);
+        leao.setOnClickListener(this);
+        macaco.setOnClickListener(this);
+        ovelha.setOnClickListener(this);
+        vaca.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch ( v.getId() ){
+            case R.id.caoId:
+                mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.cao);
+                tocaSom();
+                break;
+            case R.id.gatoId:
+                mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.gato);
+                tocaSom();
+                break;
+            case R.id.leaoId:
+                mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.leao);
+                tocaSom();
+                break;
+            case R.id.macacoId:
+                mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.macaco);
+                tocaSom();
+                break;
+            case R.id.ovelhaId:
+                mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.ovelha);
+                tocaSom();
+                break;
+            case R.id.vacaId:
+                mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.vaca);
+                tocaSom();
+                break;
+        }
+    }
+
+    private void tocaSom(){
+        if( mediaPlayer != null ){
+            mediaPlayer.start();
+        }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        if( mediaPlayer != null ) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+        super.onDestroy();
     }
 }
